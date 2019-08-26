9e91c4ef433ec9fcc51776d5dec52c5b4891e5a8
Hatice Aksoy <hatice.782.aksoy@gmail.com>
2019-08-26 17:31:42 +0300

    Analytic Dashboard Design/ Layout Designing/ Adding Charts

---
+++ Beamity.Web/Controllers/AnalyticsController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Beamity.Web.Controllers
+{
+    public class AnalyticsController : Controller
+    {
+        public IActionResult RoomsIndex()
+        {
+            return View();
+        }
+
+        public IActionResult ArtifactsIndex()
+        {
+            return View();
+
+        }
+
+        public IActionResult MuseumIndex()
+        {
+            return View();
+        }
+    }
+}
