4120cdca7091514e6e83e2a9792860102315efb8
skywarth <yigitk.ersoy@gmail.com>
2019-08-27 16:43:48 +0400

    migrate import file, delete alert

---
+++ Beamity.Web/wwwroot/js/dataTransfers/GetElementsXAverage.js
@@ -0,0 +1,27 @@
+﻿
+
+var asd;
+$(document).ready(function () {
+    swalload();
+    var options = {};
+    options.url = "https://localhost:44327/api/BeaconActivity/GetArtifactsVisitorAverage";
+    options.type = "POST";
+    var obj = {};
+    obj.LocationId = "8962C635 - D42D - 43C5 - B1E3 - FC77FCF71B19";
+    options.data = JSON.stringify(obj);
+    options.contentType = "application/json";
+    options.dataType = "html";
+    options.success = function (data, msg) {
+        $("#msg").html(msg);
+        Swal({
+            type: 'success',
+            title: 'Thanks',
+            text: 'Operation is success',
+        })
+    };
+    options.error = function () {
+        $("#msg").html("Error while calling the Web API!");
+        alert("Hata");
+    };
+    asd = $.ajax(options);
+});
