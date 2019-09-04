598f6ea186bc001515498c851a92c8b284da4366
Hatice Aksoy <hatice.782.aksoy@gmail.com>
2019-09-04 09:46:30 +0300

    End of the Rooms View

---
+++ Beamity.Web/wwwroot/js/ChartsJS/RoomsBehaviourFlow.js
@@ -0,0 +1,61 @@
+﻿
+
+//Rooms Behaviour Flow
+
+am4core.ready(function () {
+
+    // Themes begin
+    am4core.useTheme(am4themes_animated);
+    // Themes end
+
+
+
+    var chart = am4core.create("chartdiv", am4charts.ChordDiagram);
+
+
+    chart.data = [
+        { from: "A", to: "D", value: 10 },
+        { from: "B", to: "D", value: 8 },
+        { from: "B", to: "E", value: 4 },
+        { from: "B", to: "C", value: 2 },
+        { from: "C", to: "E", value: 14 },
+        { from: "E", to: "D", value: 8 },
+        { from: "C", to: "A", value: 4 },
+        { from: "G", to: "A", value: 7 },
+        { from: "D", to: "B", value: 1 }
+    ];
+
+    chart.dataFields.fromName = "from";
+    chart.dataFields.toName = "to";
+    chart.dataFields.value = "value";
+
+    // make nodes draggable
+    var nodeTemplate = chart.nodes.template;
+    nodeTemplate.readerTitle = "Click to show/hide or drag to rearrange";
+    nodeTemplate.showSystemTooltip = true;
+
+    var nodeLink = chart.links.template;
+    var bullet = nodeLink.bullets.push(new am4charts.CircleBullet());
+    bullet.fillOpacity = 1;
+    bullet.circle.radius = 5;
+    bullet.locationX = 0.5;
+
+    // create animations
+    chart.events.on("ready", function () {
+        for (var i = 0; i < chart.links.length; i++) {
+            var link = chart.links.getIndex(i);
+            var bullet = link.bullets.getIndex(0);
+
+            animateBullet(bullet);
+        }
+    })
+
+    function animateBullet(bullet) {
+        var duration = 3000 * Math.random() + 2000;
+        var animation = bullet.animate([{ property: "locationX", from: 0, to: 1 }], duration)
+        animation.events.on("animationended", function (event) {
+            animateBullet(event.target.object);
+        })
+    }
+
+}); // end am4core.ready()
