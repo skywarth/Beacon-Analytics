b909af0704b57e15a36557c139835bc80ab203b9
Hatice Aksoy <hatice.782.aksoy@gmail.com>
2019-09-05 14:16:52 +0300

    artifacts and rooms html edited

--- Beamity.Web/wwwroot/js/ChartsJS/RoomsChart1.js
+++ Beamity.Web/wwwroot/js/ChartsJS/RoomsChart1.js
@@ -1,39 +1,70 @@
-﻿
+﻿/**
+ * ---------------------------------------
+ * This demo was created using amCharts 4.
+ * 
+ * For more information visit:
+ * https://www.amcharts.com/
+ * 
+ * Documentation is available at:
+ * https://www.amcharts.com/docs/v4/
+ * ---------------------------------------
+ */
 
-//Total Visitors for one Room
+// Themes begin
+am4core.useTheme(am4themes_animated);
+// Themes end
 
+// Create chart instance
+var chart = am4core.create("chart1", am4charts.XYChart);
 
+// Add data
+chart.data = generateChartData();
 
-Highcharts.getJSON('https://www.highcharts.com/samples/data/aapl-c.json', function (data) {
+// Create axes
+var dateAxis = chart.xAxes.push(new am4charts.DateAxis());
+dateAxis.renderer.minGridDistance = 50;
 
-    // Create the chart
-    Highcharts.stockChart('chart1', {
+var valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
 
+// Create series
+var series = chart.series.push(new am4charts.LineSeries());
+series.dataFields.valueY = "visits";
+series.dataFields.dateX = "date";
+series.strokeWidth = 2;
+series.minBulletDistance = 10;
+series.tooltipText = "{valueY}";
+series.tooltip.pointerOrientation = "vertical";
+series.tooltip.background.cornerRadius = 20;
+series.tooltip.background.fillOpacity = 0.5;
+series.tooltip.label.padding(12, 12, 12, 12)
 
-        rangeSelector: {
-            selected: 2
-        },
+// Add scrollbar
+chart.scrollbarX = new am4charts.XYChartScrollbar();
+chart.scrollbarX.series.push(series);
 
-        title: {
-            text: 'AAPL Stock Price'
-        },
+// Add cursor
+chart.cursor = new am4charts.XYCursor();
+chart.cursor.xAxis = dateAxis;
+chart.cursor.snapToSeries = series;
 
-        series: [{
-            name: 'AAPL Stock Price',
-            data: data,
-            lineWidth: 0,
-            marker: {
-                enabled: true,
-                radius: 2
-            },
-            tooltip: {
-                valueDecimals: 2
-            },
-            states: {
-                hover: {
-                    lineWidthPlus: 0
-                }
-            }
-        }]
-    });
-});
+function generateChartData() {
+    var chartData = [];
+    var firstDate = new Date();
+    firstDate.setDate(firstDate.getDate() - 1000);
+    var visits = 1200;
+    for (var i = 0; i < 500; i++) {
+        // we create date objects here. In your data, you can have date strings
+        // and then set format of your dates using chart.dataDateFormat property,
+        // however when possible, use date objects, as this will speed up chart rendering.
+        var newDate = new Date(firstDate);
+        newDate.setDate(newDate.getDate() + i);
+
+        visits += Math.round((Math.random() < 0.5 ? 1 : -1) * Math.random() * 10);
+
+        chartData.push({
+            date: newDate,
+            visits: visits
+        });
+    }
+    return chartData;
+}
